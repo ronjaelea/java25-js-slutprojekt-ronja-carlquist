53f81d7221f62772586ff8ea6cb28f0ff7c3d15c
Ronja Carlquist <ronja@Ronjas-MacBook-Air.local>
2026-05-16 17:13:55 +0200

    Add basic styling for index page

--- js/render.js
+++ js/render.js
@@ -3,7 +3,9 @@ import { IMAGE_BASE_URL } from "./config.js";
 
 export const createMovieCard = (movie) => {
     const card = document.createElement('div');
-    // card.className = css-regel för card
+    card.className = 'card';
+    const upper = document.createElement('div');
+    const lower = document.createElement('div');
 
     const poster = document.createElement('img');
     poster.src = `${IMAGE_BASE_URL}${movie.poster_path}`
@@ -15,7 +17,7 @@ export const createMovieCard = (movie) => {
     releaseDate.textContent = `Release date: ${movie.release_date}`;
 
     const rating = document.createElement('p');
-    rating.textContent = `Average score: ${movie.vote_average} / 10`;
+    rating.textContent = `Average score: ${movie.vote_average.toFixed(1)} / 10`;
 
     // beskrivning, endast vid sökresultat
     // if (isSearchResult...) {
@@ -23,14 +25,16 @@ export const createMovieCard = (movie) => {
     //     info.textContent...
     // }
 
-    card.append(poster, title, releaseDate, rating);
+    upper.append(poster, title);
+    lower.append(releaseDate, rating);
+    card.append(upper, lower);
     return card;
 };
 
 
 export const createPersonCard = (person) => {
     
-};
+};  
 
 export const renderCards = (movies, list) => {
     console.log(movies);
@@ -39,6 +43,5 @@ export const renderCards = (movies, list) => {
         const card = createMovieCard(movie);
         container.append(card);
     });
-
 }
 
